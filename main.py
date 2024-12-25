06592af2bc678bab9da0df41abb011f8e6fdac65
Dimon <Kachalin_dmitriy@mail.ru>
2024-12-25 13:49:39 +0300

    25.12

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 
+#"1st program"
+print(9 ** 0.5 * 5)
 
-print("Hello, world!")
-print("I'm Dima")
-print("I am")
+#"2nd program"
+print(9.99>9.98 and 1000!=1001)
+
+#"3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+#"4th program"
+print('123.456')
+print(123.456)
+print(123.456 * 10)
+print(int(1234.56%10))
